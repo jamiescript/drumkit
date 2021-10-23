988f696dfed7f8a60ffdc5100ffb5763519a7da2
Jamie Piper <jamiepiperjavascript@gmail.com>
2021-10-23 21:05:16 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Key } from "react";
+import Kit from "./kit-data";
+import "./App.css";
 
 function App() {
+  function removeTransition(e: any) {
+    console.log("works");
+    if (e.propertyName !== "transform") return; // only trigger on transform property
+    e.target.classList.remove("playing"); // remove 'playing' class name
+  }
+
+  function playSound(e: any) {
+    const audio = document.querySelector(
+      `audio[data-key="${e.keyCode}"]`
+    ) as HTMLAudioElement;
+    if (!audio) return; // if no corresponding audio element, return
+    const key = document.querySelector(
+      `div[data-key="${e.keyCode}"]`
+    ) as HTMLElement;
+    if (!key) return; // if no corresponding key element, return
+
+    key.classList.add("playing"); // add 'playing' class name
+    audio.currentTime = 0; // allow for rapid keydown
+    console.log(audio);
+    // audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll(".key"));
+  keys.forEach(
+    (key) => key.addEventListener("transitionend", removeTransition) // reset key when transition has ended
+  );
+  window.addEventListener("keydown", playSound);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="keys">
+        {Kit.map((item: any, key: Key) => {
+          return (
+            <div
+              tabIndex={0}
+              onKeyDown={playSound}
+              data-key={item.dataKey}
+              key={key}
+              className="key"
+            >
+              <kbd>{item.key}</kbd>
+              <span className="sound">{item.sound}</span>
+              <audio data-key={item.dataKey} src={item.src}></audio>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
